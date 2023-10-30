d13c16667c7030c1574ac586a6b4164cb02de342
PaulScollay <117660903+PaulScollay@users.noreply.github.com>
2023-10-30 12:31:42 +0000

    Sanity Only

--- app/page.js
+++ app/page.js
@@ -17,7 +17,54 @@ export default async function Page() {
   console.log(projectsRes)
   console.log(projectsRes.docs[0].locations)
   console.log(projectsRes.docs[0].optionsTab)
-  console.log(projectsRes.docs[0])
+  console.log(projectsRes.docs[0].unitType)
+
+//   const { BlobServiceClient } = require("@azure/storage-blob");
+
+//   // Load the .env file if it exists
+//   require("dotenv").config();
+
+
+//   const STORAGE_CONNECTION_STRING = process.env.STORAGE_CONNECTION_STRING || "";
+
+//   // Note - Account connection string can only be used in node.
+//  const blobServiceClient = BlobServiceClient.fromConnectionString(STORAGE_CONNECTION_STRING);
+
+//   // List blobs in container
+//   try {
+//     // Create container client
+//     const containerClient = await blobServiceClient.getContainerClient(
+//       "images"
+//     );
+
+//     //const blobName = "coastfields3.jpeg"
+//     const blockBlobClient = containerClient.getBlockBlobClient("coastfields3.jpeg")
+//     //const downloadBlockBlobResponse = await blockBlobClient.download(0);
+//     //blockBlobClient.urlq
+//     // do something with containerClient...
+//     let i = 1;
+//     console.log(blockBlobClient.url)
+
+
+
+//     // List blobs in container
+//     for await (const blob of containerClient.listBlobsFlat({
+//       includeMetadata: true,
+//       includeSnapshots: false,
+//       includeTags: true,
+//       includeVersions: false,
+//       prefix: ''
+//     })) {
+//       //console.log(`Blob ${i++}: ${blob.name} ${blob.metadata} }`);
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     throw err;
+//   }
+  // for await (const container of blobServiceClient.listContainers()) {
+  //   console.log(`Container ${i++}: ${container.name}`);
+  // }
+
 
   return (
         <div>
